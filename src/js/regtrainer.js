789284370329d3ec5fcab61fed08e5db03136033
olekern <ole.ekern@gmail.com>
2017-03-23 14:23:39 +0100

    Changed registration of team to happen in cloud code

--- src/js/regtrainer.js
+++ src/js/regtrainer.js
@@ -5,8 +5,6 @@ Parse.serverURL = 'https://klubbenheroku.herokuapp.com/parse';
             
             Parse.User.logOut();
             
-            var registerTeams = Parse.Object.extend("RegisterTeams");
-            var setTeam = Parse.Object.extend("Teams");
             var teamName = document.reg.elements[0].value;
             var coachName = document.reg.elements[1].value;
             var coachMail = document.reg.elements[2].value;
@@ -23,75 +21,38 @@ Parse.serverURL = 'https://klubbenheroku.herokuapp.com/parse';
             else {
                 if (document.getElementById('terms').checked) {
                     document.getElementById('reg').style.cssText = "display: none;";
-                    var team = new registerTeams();
-                    team.set("team", teamName);
-                    team.set("coachname", coachName);
-                    team.set("coachmail", coachMail);
-                    /*
-                    team.set("mailinvoice", invMail);
-                    team.set("phoneinvoice", invMob)
-                    team.set("address", address);
-                    team.set("postal", postal);
-                    */
-                    team.save(null, {
-                        success: function (gameScore) {
-                            var teamSet = new setTeam();
-                            teamSet.set("Name", teamName);
-                            teamSet.save(null, {
-                                
-                                success: function (gameScore) {
-                                    var teamId;
-                                    var teamiD; 
-                                    var query = new Parse.Query(setTeam);
-                                    query.equalTo("Name", teamName);
-                                    query.find({
-                                        success: function(results) {
-                                            var teamid = results;
-                                            teamiD = teamid[0];
-                                            teamId = teamiD.id;
-                                            console.log(teamiD);
-                                        }
-
-                                    });
-                                    
-                                    var user = new Parse.User();
-                                    user.set("name", coachName);
-                                    user.set("email", coachMail);
-                                    user.set("username", coachMail)
-                                    user.set("password", coachPass);
-                                    user.addUnique("teams", teamSet);
-                                    user.set("team", teamSet);
-                                    user.signUp(null, {
-                                            success: function(user) {
-                                                console.log("New user signed up successfully!");
-                                                function logIn() {
+                    
+                    var user = new Parse.User();
+                    user.set("name", coachName);
+                    user.set("email", coachMail);
+                    user.set("username", coachMail)
+                    user.set("password", coachPass);
+                    user.signUp(null, {
+                            success: function(user) {
+                                console.log("New user signed up successfully!");
+                                function logIn() {
                                     var email = coachMail;
                                     var passord = coachPass;
 
                                     Parse.User.logIn(email, passord, {
                                             success: function() {
                                                 console.log("Innlogging  suksessfull");
-                                                
-                                                var newTeam = Parse.Object.extend("data_" + teamId+ "_Members");
-                                                var teamNew = new newTeam();
-
-                                                teamNew.set("user", Parse.User.current());
-                                                teamNew.set("role", "trener");
-                                                teamNew.set("accepted", true);
-                                                teamNew.save(null, {
-                                                            success: function () {
-                                                                console.log("Signed up team");
-                                                                alert('Bruker registrert. Logg inn for å begynne å bruke systemet.');
-                                                                location.href = "./index.html";
-                                                            }
-                                                            , error: function (error) {
-                                                                alert('Problem ved registrering');
-                                                            }
-                                                        });
+                                                var userId = Parse.User.current().id;
+                                                console.log(teamName);
+                                                console.log(userId);
+                                                Parse.Cloud.run("registerTeam", {
+                                                teamName: teamName,
+                                                userId: userId
+                                                }).then(function(response){
+                                                    document.getElementById('reg').style.cssText = "display: block;";
+                                                    alert("Ditt nye lag ble nå opprettet. Logg inn med brukeren din for å begynne");
+                                                    location.href = "./src/html/registration.html";
+                                                });
                                                 
                                             },
                                             error: function() {
                                                 console.log("Innlogging feilet:" + error.message);
+                                                document.getElementById('reg').style.cssText = "display: block;";
                                             }
                                         })
                                     return false;
@@ -102,22 +63,10 @@ Parse.serverURL = 'https://klubbenheroku.herokuapp.com/parse';
                                 error: function(user, error) {
                                     console.log("Error: " + error.code + "" + error.message);
                                     alert("Her skjedde det visst en liten feil: " + error.message);
+                                    document.getElementById('reg').style.cssText = "display: block;";
                                 }
                             });
-                                    
-                                    
-                                }
-                                , error: function (gameScore, error) {
-                                    alert('Problem registrering');
-                                }
-                                });
-                        }
-                        , error: function (gameScore, error) {
-                            alert('Problem');
-                        }
-                    });
-                    
-                    
+                             
                     
         } else{
             alert('Du må bekrefte at du har lest og akseptert våre vilkår og betingelser før du kan registrere deg.');
